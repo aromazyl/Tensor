902d326a555f0df2864330e7df85e7c80420ce99
aromazyl <zyl2336709@gmail.com>
2017-10-29 20:53:21 +0800

    add tensor traits

---
+++ tensor_traits.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+template <int k, typename T, class TensorType>
+class TensorTraits;
+
+template <typename T, class TensorType>
+class TensorTraits<1, T, TensorType> {
+public:
+  T norm() const;
+};
+
+template <typename T, class TensorType>
+class TensorTraits<2, T, TensorType> {
+public:
+  size_t rows() const;
+  size_t columns() const;
+};
