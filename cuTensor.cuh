01287bf85efb65e90f9e5858d958e9d5c3fffb50
aromazyl <zyl2336709@gmail.com>
2017-11-01 18:22:31 +0800

    update

--- cuTensor.cuh
+++ cuTensor.cuh
@@ -3,7 +3,46 @@
 
 namespace tensor {
 
+
+
+// cuda function support implementation
+namespace {
+}
+
+template <int k, typename T>
 struct cuTensor {
-  
+  typedef TensorTraits<k, T, cuTensor> traits_type;
+  typedef T value_type;
+  typedef T* pointer;
+  typedef const T* const_pointer;
+  typedef T& reference;
+  typedef const T& const_reference;
+
+  size_t n_[k] = {0};
+  pointer data_;
+  bool wrapped_;
+
+  constexpr static int rand() { return k; };
+  pointer data() { return data_; }
+  const_pointer data() { return data_; }
+  cuTensor() : data_(NULL), wrapped_() {}
+
+  cuTensor(const cuTensor& a);
+
+  cuTensor(cuTensor&& src);
+
+  ~cuTensor() {
+    if (!wrapped_) {
+      checkCudaErrors(cudaFree(data_));
+    }
+  }
+
+  cuTensor &operator=(const cuTensor& src);
+
+  cuTensor &operator=(cuTensor&& src);
+
+  size_t (*init_dim)(void);
+
 };
+
 }
